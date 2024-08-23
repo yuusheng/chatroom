b46dbc5158494bb1202743201a04d29a416f1c43
yuusheng <hi@yuusheng.im>
2024-08-23 22:42:38 +0800

    feat(ui): header

--- apps/chatroom-client/src/components/Header/index.tsx
+++ apps/chatroom-client/src/components/Header/index.tsx
@@ -1,21 +1,35 @@
 import { onlineCountAtom, showUserJoinedAtom } from "~/atoms";
 import { useAtom, useAtomValue } from "jotai";
 import { Switch } from "~/components/ui/switch";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
 
 export function Header() {
   const onlineCount = useAtomValue(onlineCountAtom);
   const [showUserJoin, setShowUserJoin] = useAtom(showUserJoinedAtom)
 
   return (
-    <header className="w-full h-10 py-1 flex">
-      <div className="w-16 px-4 py-0.5 flex rounded-3xl bg-lime-200/30  items-center justify-between">
-        <div className="h-1.5 w-1.5 bg-green-600 shadow-green-600 shadow rounded-full"></div>
-        <span>{onlineCount}</span>
-      </div>
+    <header className="w-full h-10 py-2 px-5 flex items-center">
+      <div className="flex-1" />
+      <div className="flex items-center gap-3">
+        <div className="w-14 h-7 px-4 py-0.5 flex rounded-3xl bg-lime-200/30  items-center justify-between">
+          <div className="h-1.5 w-1.5 bg-green-600/20 rounded-full relative">
+            <div className="h-1.5 w-1.5 bg-green-600 blur-[1px]  rounded-full absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+          </div>
+          <span className="">{onlineCount}</span>
+        </div>
 
-      <div className="flex gap-2 items-center">
-        <span>User Join Toast</span>
-        <Switch checked={showUserJoin} onCheckedChange={setShowUserJoin} />
+        <div className="flex gap-2 items-center">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger >
+                <Switch checked={showUserJoin} onCheckedChange={setShowUserJoin} />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>User Join Toast</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
       </div>
     </header>
   )
