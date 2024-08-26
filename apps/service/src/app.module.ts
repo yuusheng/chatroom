7ee3d9fce7291c30fd0b946aa578827f06916524
yuusheng <hi@yuusheng.im>
2024-08-26 23:28:59 +0800

    feat: supabase setup

--- apps/service/src/app.module.ts
+++ apps/service/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common'
 import { ChatModule } from 'chat/chat.module'
 import { UserModule } from 'user/user.module'
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ChatModule, UserModule],
+  imports: [ChatModule, UserModule, AuthModule,
+    ConfigModule.forRoot()
+  ],
   providers: [],
 })
 export class AppModule  {}
