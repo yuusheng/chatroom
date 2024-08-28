916353de48aa15a73676dbb20a046de2ef99efe7
yuusheng <hi@yuusheng.im>
2024-08-28 22:30:59 +0800

    chore: reorder files

--- apps/service/src/app.module.ts
+++ apps/service/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ChatModule } from 'chat/chat.module'
 import { UserModule } from 'user/user.module'
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ChatModule, UserModule, AuthModule,
+  imports: [ChatModule, UserModule,
     ConfigModule.forRoot(),
     DatabaseModule
   ],
