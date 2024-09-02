3eef65c3d73da8c59b644db64ad764716a8f0250
yuusheng <hi@yuusheng.im>
2024-09-02 23:37:46 +0800

    lint: drizzle

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,20 @@
 import antfu from '@antfu/eslint-config'
+import * as drizzle from 'eslint-plugin-drizzle'
 
-export default antfu({
-  jsx: true,
-  typescript: true,
-  ignores: ['**/migrations', '**/ui'],
-})
+export default antfu(
+  {
+    jsx: true,
+    react: true,
+    typescript: true,
+    ignores: ['**/migrations', '**/ui'],
+  },
+  {
+    name: 'drizzle',
+    files: ['**/database/**/*.ts'],
+    plugins: { drizzle },
+    rules: {
+      'drizzle/enforce-delete-with-where': ['error', { drizzleObjectName: 'db' }],
+      'drizzle/enforce-update-with-where': ['error', { drizzleObjectName: 'db' }],
+    },
+  },
+)
